d7b3c6ef45b8ccdadeca69b12839f555c4032855
island205 <island205@gmail.com>
2013-09-04 10:47:28 +0800

    remove duplicated code

--- dev/go.js
+++ dev/go.js
@@ -104,7 +104,6 @@ queue(function (cb) {
   var config = {}
   config.couchAuth = 'admin:admin'
   config.registryCouch = 'http://localhost:15984/'
-  config.registryCouch = 'http://localhost:15984/'
   config.redis = { host: '127.0.0.1', port: 16379 }
   config.redisAuth = 'i-am-using-redis-in-development-mode-for-npm-www'
   config.keys = ['these keys are for dev mode only']
