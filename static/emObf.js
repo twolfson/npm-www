6d6f1641066b2cfdbfe5707386cfc9e23316da90
rockbot <raquel@rckbt.me>
2014-03-07 10:42:43 -0800

    adds support for IDN email addresses

--- static/emObf.js
+++ static/emObf.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   var e = $('#email')
-  var em = decodeURIComponent(e.data('email'))
+  var em = e.data('email').split('%').slice(1).map(function (x) { 
+              return String.fromCharCode(parseInt(x, 16)) 
+            }).join('')
   e.html('<a href="mailto:' + em + '">' + em + '</a>')
 })
 
