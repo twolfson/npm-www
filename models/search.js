7109a8c49f6e592d383cd5ffc631fb4e2c4f9c8a
rockbot <raquel@rckbt.me>
2014-03-25 13:25:51 -0700

    only shows arrows when DLs significantly (in|de)crease

--- models/search.js
+++ models/search.js
@@ -22,7 +22,7 @@ function search(params, cb) {
   var url = config.elasticsearch.url + '/package/_search?' + querystring.stringify(qs)
 
   var payload = {
-    "fields": ["name", "keywords", "description", "author", "version", "stars", "dlScore", "dlDay"],
+    "fields": ["name", "keywords", "description", "author", "version", "stars", "dlScore", "dlDay", "dlWeek"],
     "query": {
       "function_score": {
         "query": {
